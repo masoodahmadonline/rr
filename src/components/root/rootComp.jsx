c336c126a8aba37b0ff0e94d1f76123ee65165d6
Masood <masood@masood.com>
2019-08-17 15:21:49 +0300

    linting, header,footer routes

--- src/components/root/rootComp.jsx
+++ src/components/root/rootComp.jsx
@@ -1,11 +1,1 @@
-
-// import {showErrors as ShowErrors} from "../UtilComp/wiseMessageContainer";
-export const rootComp = (props) => {
-  return (
-    <div>
-
-      {/*<ShowErrors />*/}
-      <h1>this is root</h1>
-    </div>
-  );
-};
+import React, {Component} from "react";
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,47 @@
+import {Dashboard} from "./components/common/dashboard";
+import {Footer} from "./components/common/footer";
+import {Header} from "./components/common/header";
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
-import {rootComp as Root} from "./components/root/rootComp";
+import {SideBar} from "./components/common/sidebar";
+import PropTypes from "prop-types";
 import React from "react";
-export const routes = () => (
 
+// eslint-disable-next-line react/no-multi-comp
+function Layout({ children }){
+  return <><Header/><SideBar/>{children}<Footer/></>;
+}
+Layout.propTypes = {
+  children : PropTypes.object
+};
+
+const LayoutRoute = ({ path, ...props }) => {
+  return <Route path={path} render={() => <Layout><Route {...props} /></Layout>} />;
+};
+
+LayoutRoute.propTypes = {
+  path : PropTypes.object
+};
+
+export const routes = () => (
   <Router>
     <div>
       <Switch>
-      <Route path="/other" component={() => <div>other</div>} />
-      <Route path="/other2" component={() => <div>other2</div>} />
-      <Route path="/" component={Root} />
+        <Route path="/signin" render={() => "This is signin page without header or footer"}/>
+        <LayoutRoute path="/dashboard" component={Dashboard}/>
       </Switch>
     </div>
   </Router>
+
 );
+
+
+{/*<Router>*/}
+{/*  <div>*/}
+{/*    <Switch>*/}
+{/*      <Route path="/other" component={() => <div>other</div>} />*/}
+{/*      <Route path="/other2" component={() => <div>other2</div>} />*/}
+{/*      <Route path="/" component={Root} />*/}
+{/*    </Switch>*/}
+{/*  </div>*/}
+{/*</Router>*/}
 
